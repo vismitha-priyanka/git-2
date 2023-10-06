1fdb592af1a3ed94ea1897566b3af5bf6713cef0
vismitha-priyanka <130166239+vismitha-priyanka@users.noreply.github.com>
2023-10-06 16:46:07 +0530

    Add files via upload

---
+++ gfg.py
@@ -0,0 +1,13 @@
+"""
+This is a simple script to add two numbers and print the result.
+"""
+
+# Assign values to variables
+A = 1
+B = 2
+
+# Calculate the sum
+RESULT = A + B
+
+# Print the result
+print("The sum of A and B is:", RESULT)
---
+++ py_test.py
@@ -0,0 +1,9 @@
+# test_math_functions.py
+
+def add(a, b):
+    return a + b
+
+def test_addition():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
